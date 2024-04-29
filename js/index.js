e29c96bb26b2efe5df6e8d42b13ca37dc918a9ba
Favour <117774204+Clara4555@users.noreply.github.com>
2024-04-29 22:13:06 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+        
+    
+
+
+
+
+
+function scrollToLeft() {
+    const container = document.querySelector('.icons');
+    container.scrollLeft -= 100; // Adjust scroll speed as needed
+}
+
+function scrollToRight() {
+    const container = document.querySelector('.icons');
+    container.scrollLeft += 100; // Adjust scroll speed as needed
+}
+
